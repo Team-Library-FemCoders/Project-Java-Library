4a02dd6dec3ff7085963da5528255829c87329d1
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-05-29 14:41:19 +0200

    feat(Book): add attributes, methods and constructors

---
+++ src/main/java/org/example/model/Book.java
@@ -0,0 +1,88 @@
+package org.example.model;
+
+public class Book {
+    private String title;
+    private String author;
+    private String summary;
+    private String genre;
+    private String isbn;
+    private int id;
+
+    public Book(String title, String author, String summary, String genre, String isbn, int id) {
+        this.title = title;
+        this.author = author;
+        this.summary = summary;
+        this.genre = genre;
+        this.isbn = isbn;
+        this.id = id;
+    }
+
+    public Book(String title, String author, String summary, String genre, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.summary = summary;
+        this.genre = genre;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String printBook() {
+        return "Book{Title: '" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", summary='" + summary + '\'' +
+                ", genre='" + genre + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", id=" + id +
+                '}';
+
+//        "Title: Nombre\tAuthor: autor\tGenre: genre\"
+//        "Title: Nombre\tAuthor: autor\tGenre: genre\"
+//
+//        Título          Género          Autor isbn id
+//            Nom             Lit
+    }
+}
