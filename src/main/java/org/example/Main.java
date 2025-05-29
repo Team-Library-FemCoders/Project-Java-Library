112eb5bf939c7f768b0f192b3de6c75c3538852e
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-05-29 15:33:00 +0200

    feat(BookController): add BookController class and findAllController method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.config.DBManager;
+import org.example.controller.BookController;
 import org.example.model.Book;
 import org.example.repository.BookRepository;
 
@@ -13,8 +14,9 @@ public class Main {
 //        Book book = new Book("hola", "hola", "hola", "hola", "hola", 3);
 //        System.out.println(book.describeBook());
 
-        BookRepository br = new BookRepository();
-        br.findAll();
+        BookRepository bookRepository = new BookRepository();
+        BookController bookController = new BookController(bookRepository);
+        bookController.findAllController();
 
     }
 }
---
+++ src/main/java/org/example/controller/BookController.java
@@ -0,0 +1,18 @@
+package org.example.controller;
+
+import org.example.model.Book;
+import org.example.repository.BookRepository;
+
+import java.util.List;
+
+public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> findAllController(){
+        return this.bookRepository.findAll();
+    }
+}
