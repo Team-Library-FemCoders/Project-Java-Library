c2e3ebe5a155fb7f56f67e3c6fe15f9520342736
Lara Pla <laraplamoreno@gmail.com>
2025-06-03 15:10:49 +0200

    feat(App): add attributes, constructor and startMenu method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,37 +11,12 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//        BookRepository bookRepository = new BookRepository();
+//        BookController bookController = new BookController(bookRepository);
+//        BookView bookView= new BookView(bookController);
+        App app = new App();
 
-        Book book = new Book("El último Catón", "Matilde Asensi", "Bajo el suelo de la Ciudad del Vaticano, la hermana Ottavia Salina, recibe el encargo de descifrar unas extrañas escarificaciones.", "Ficción", "9788408081715");
-//        System.out.println(book.describeBook());
-
-        BookRepository bookRepository = new BookRepository();
-        BookController bookController = new BookController(bookRepository);
-        //bookController.findAllController();
-        BookView bookView= new BookView(bookController);
-        //bookView.showOneBook(book);
-
-        //bookView.deleteBookView();
-
-        //bookView.saveBookView();
-
-        //bookView.formSelectBookId();
-
-//        bookView.showBooks();
-//        Book book1 =bookRepository.selectOneBookById(1);
-//        bookView.showOneBook(book1);
-//
-//        book1.setTitle("uuuuuu");
-//        bookView.showBooks();
-//
-//        bookRepository.updateBook(book1);
-//        bookView.showOneBook(book1);
-
-//        Scanner scanner = new Scanner(System.in);
-//        String resulte = bookView.askAttribute("bubu",10,"title",scanner);
-//        System.out.println(resulte);
-
-        bookView.updateBookView();
+        app.startMenu();
 
 
     }
